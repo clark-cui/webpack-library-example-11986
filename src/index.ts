8187351cbad9f59c9874c00ce9e3b73ec1c4ecaf
clark-cui <cuirongchuan@163.com>
2021-01-15 23:56:14 +0800

    add test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export function mount() {}
-export function bootstrap() {}
-export function unmount() {}
+export function mount() {
+    console.log("mount")
+}
+export function bootstrap() {
+    console.log("bootstrap")
+
+}
+export function unmount() {
+    console.log("unmount")
+
+}
